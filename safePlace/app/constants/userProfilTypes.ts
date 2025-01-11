0b11dbcac57f2cad9befb94b3da392bfc75c7519
mgumis15 <49537639+mgumis15@users.noreply.github.com>
2025-01-11 21:12:21 +0100

    User types added

---
+++ safePlace/app/constants/userProfilTypes.ts
@@ -0,0 +1,23 @@
+export const USER_EXPIERIENCE_TYPES = [
+  "None",
+  "Little Experience",
+  "Experienced",
+  "Professional",
+] as const;
+
+export const USER_LIFESTYLE_TYPES = [
+  "Sedentary lifestyle",
+  "Moderately Active Lifestyle",
+  "Active lifestyle",
+] as const;
+
+export const HOUSING_TYPES = [
+  "House",
+  "Apartment",
+] as const;
+
+export const HOUSING_AREA_TYPES = [
+  "Countryside",
+  "Small town",
+  "Big town",
+] as const;
