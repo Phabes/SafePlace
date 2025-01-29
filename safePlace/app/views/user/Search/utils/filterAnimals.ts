f4f1b42bc3258139144a8589a946b7bc93356d4d
Phabes <quinlanvos1234@gmail.com>
2025-01-29 10:14:29 +0100

    Filter updates.

--- safePlace/app/views/user/Search/utils/filterAnimals.ts
+++ safePlace/app/views/user/Search/utils/filterAnimals.ts
@@ -1,46 +1,88 @@
 import { Filter } from "../../../../constants/filterType";
 import { AdditionalUserData, AnimalDB } from "../../../../types";
 
-export const filterAnimals = (animals: Array<AnimalDB>, filters: Filter, userDetails?:AdditionalUserData) =>{
+export const filterAnimals = (
+  animals: Array<AnimalDB>,
+  filters: Filter,
+  userDetails?: AdditionalUserData
+) => {
   const filteredAnimals = animals
-  .filter((animal)=>(filters.type == undefined || filters.type == animal.type))
-  .filter((animal) => (filters.environment == undefined || filters.environment == animal.environment))
-  .filter((animal) => (filters.friendly == undefined || filters.friendly == animal.friendly))
-  .filter((animal) => (filters.age == undefined || filters.age >= animal.age))
+    .filter(
+      (animal) => filters.type == undefined || filters.type == animal.type
+    )
+    .filter(
+      (animal) =>
+        filters.environment == undefined ||
+        filters.environment == animal.environment
+    )
+    .filter(
+      (animal) =>
+        filters.friendly == undefined || filters.friendly == animal.friendly
+    )
+    .filter((animal) => filters.age == undefined || filters.age >= animal.age);
 
+  const userFilterdAnimals = filters.useUserDetails
+    ? filteredAnimals
+        .filter((animal) => userAgeFilter(animal, userDetails))
+        .filter((animal) => userExpierienceFilter(animal, userDetails))
+        .filter((animal) => userHousingFilter(animal, userDetails))
+        .filter((animal) => userLifestyleFilter(animal, userDetails))
+    : filteredAnimals;
 
-  const userFilterdAnimals = filters.useUserDetails ? filteredAnimals
-    .filter((animal) => userAgeFilter(animal, userDetails))
-    .filter((animal) => userExpierienceFilter(animal, userDetails))
-    .filter((animal) => userHousingFilter(animal, userDetails))
-    .filter((animal) => userLifestyleFilter(animal, userDetails))
-  :filteredAnimals
+  return userFilterdAnimals;
+};
 
-  return userFilterdAnimals
-}
+const userAgeFilter = (
+  animal: AnimalDB,
+  userDetails?: AdditionalUserData
+): boolean => {
+  if (userDetails?.age == undefined || userDetails?.age == 0) return true;
+  return (
+    (userDetails.age > 70 &&
+      animal.friendly &&
+      ["Cat", "Turtle", "Fish"].includes(animal.type)) ||
+    userDetails.age < 70
+  );
+};
 
-const userAgeFilter = (animal:AnimalDB, userDetails?:AdditionalUserData): boolean => {
-  if (userDetails?.age == undefined || userDetails?.age == 0)
-    return true
-  return (userDetails.age > 70 && animal.friendly && ["Cat", "Turtle", "Fish"].includes(animal.type))
-}
-
-const userExpierienceFilter = (animal: AnimalDB, userDetails?: AdditionalUserData): boolean => {
+const userExpierienceFilter = (
+  animal: AnimalDB,
+  userDetails?: AdditionalUserData
+): boolean => {
   if (userDetails?.experience == undefined || userDetails?.experience == "")
     return true;
-  return (["None", "Little Experience"].includes(userDetails.experience)  && animal.friendly);
+  return (
+    (["None", "Little Experience"].includes(userDetails.experience) &&
+      animal.friendly) ||
+    !["None", "Little Experience"].includes(userDetails.experience)
+  );
 };
 
-const userHousingFilter = (animal: AnimalDB, userDetails?: AdditionalUserData): boolean => {
+const userHousingFilter = (
+  animal: AnimalDB,
+  userDetails?: AdditionalUserData
+): boolean => {
   if (userDetails?.housing == undefined || userDetails?.housing == "")
     return true;
-  return (userDetails.housing == "Apartment" && ["Land","Water"].includes(animal.environment));
+  return (
+    (userDetails.housing == "Apartment" &&
+      ["Land", "Water"].includes(animal.environment)) ||
+    userDetails.housing != "Apartment"
+  );
 };
 
-const userLifestyleFilter = (animal: AnimalDB, userDetails?: AdditionalUserData): boolean => {
+const userLifestyleFilter = (
+  animal: AnimalDB,
+  userDetails?: AdditionalUserData
+): boolean => {
   if (userDetails?.lifestyle == undefined || userDetails?.lifestyle == "")
     return true;
-  return (userDetails.lifestyle == "Sedentary lifestyle" && ["Fish","Turtle"].includes(animal.type)) 
-    || (userDetails.lifestyle == "Moderately Active Lifestyle" && ["Fish", "Turtle","Cat", "Parrot"].includes(animal.type))
-    || (userDetails.lifestyle == "Active lifestyle" && ["Fish", "Turtle", "Cat", "Parrot", "Dog"].includes(animal.type));
+  return (
+    (userDetails.lifestyle == "Sedentary lifestyle" &&
+      ["Fish", "Turtle"].includes(animal.type)) ||
+    (userDetails.lifestyle == "Moderately Active Lifestyle" &&
+      ["Fish", "Turtle", "Cat", "Parrot"].includes(animal.type)) ||
+    (userDetails.lifestyle == "Active lifestyle" &&
+      ["Fish", "Turtle", "Cat", "Parrot", "Dog"].includes(animal.type))
+  );
 };
